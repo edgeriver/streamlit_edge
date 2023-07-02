d1f4f2f3d65eb29bdf47db4953b6ceccaa0dcadf
edge <wangweilong@xbmail.com.cn>
2023-07-02 18:48:13 +0800

    更改名称

--- pages/西部行业因子配置生成.py
+++ pages/西部行业因子配置生成.py
@@ -66,7 +66,7 @@ def xibu_factor(data: pd.DataFrame):
     buffer = io.BytesIO()
     with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
         all_res.to_excel(writer, sheet_name='Sheet1', index=False)
-        writer.close()
+        # writer.close()
 
     download2 = st.download_button(
         label="Download data as Excel",
